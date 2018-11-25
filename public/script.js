9b61c81ed55cfeba85c7b6ab79e4240ee806b1a0
Alexei Ptitchkin <ptitchkin@gmail.com>
2018-11-25 17:12:14 -0500

    - display quantity beside item

--- public/script.js
+++ public/script.js
@@ -3,10 +3,10 @@ new Vue({
 	data: {
 		total: 0,
 		items: [
-			{title: 'Item 1', price: 1},
-			{title: 'Item 2', price: 22},
-			{title: 'Item 3', price: 33},
-			{title: 'Item 4', price: 10},
+			{title: 'Item 1', price: 1, qty:0},
+			{title: 'Item 2', price: 22, qty:0},
+			{title: 'Item 3', price: 33, qty:0},
+			{title: 'Item 4', price: 10, qty:0},
 		],
 		cart: []
 	},
